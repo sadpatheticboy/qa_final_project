48a3e97037c3a949fe752549669cc26832915f28
sadpatheticboy <bal-tim@mail.ru>
2023-08-11 09:05:42 +0300

    some allure changes

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -6,6 +6,7 @@ def test_guest_can_go_to_login_page(browser):
     link = "https://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
+
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
@@ -15,4 +16,5 @@ def test_guest_should_see_login_link(browser):
     link = "https://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
+
     page.should_be_login_link()
--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -5,6 +5,7 @@ def test_guest_can_add_product_to_basket(browser):
     link = "https://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
+
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_correct_price()
