56b3a19fbf42aab0a30fffb63579d75b480c12fa
sadpatheticboy <bal-tim@mail.ru>
2023-08-14 09:45:33 +0300

    add mark "need_review" for review

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -5,8 +5,10 @@ from .locators import BasketPageLocators
 class BasketPage(BasePage):
     def is_basket_empty(self):
         basket_message_value = self.browser.find_element(*BasketPageLocators.BASKET_MESSAGE).text
-        assert "Your basket is not empty" in basket_message_value
+        assert "Your basket is empty" in basket_message_value, \
+            "Basket is not empty"
 
     def is_basket_not_empty(self):
         basket_message_value = self.browser.find_element(*BasketPageLocators.BASKET_MESSAGE).text
-        assert "Your basket is empty" not in basket_message_value
+        assert "Your basket is empty" not in basket_message_value, \
+            "Basket is empty"
--- test_product_page.py
+++ test_product_page.py
@@ -5,6 +5,20 @@ import pytest
 import time
 
 
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+    page = ProductPage(browser, link)
+    page.open()
+
+    page.add_product_to_basket()
+    # page.solve_quiz_and_get_code() # Здесь вызывается метод для решения математической задачи. Я решил
+    # закомментировать это, т.к. уже нет необходимости в данном методе, но пусть будет в случае чего, может захочу
+    # в будущем перепройти курс
+    page.should_be_correct_price()
+    page.should_be_correct_name()
+
+
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "https://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
@@ -33,6 +47,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -41,6 +56,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "https://selenium1py.pythonanywhere.com/en-gb/"
     page = ProductPage(browser, link)
@@ -63,13 +79,13 @@ class TestUserAddToBasketFromProductPage:
         page.register_new_user(email, 'adinWQye236sao')
         page.should_be_authorized_user()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
         page = ProductPage(browser, link)
         page.open()
 
         page.add_product_to_basket()
-        # page.solve_quiz_and_get_code()
         page.should_be_correct_price()
         page.should_be_correct_name()
 
